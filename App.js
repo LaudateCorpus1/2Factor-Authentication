f0af9a165132dc33840180f6cd49e0ffe993bc4f
Saad <saadbennmushtaq@gmail.com>
2018-03-11 12:57:10 -0500

    Set up a basic UI for signup

--- App.js
+++ App.js
@@ -1,58 +1,85 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   Platform,
   StyleSheet,
   Text,
   View
+  Button,
+  TextInput
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import Amplify, { Auth } from 'aws-amplify'
+import config from './src/aws-exports'
+Amplify.configure(config)
+
 
 type Props = {};
 export default class App extends Component<Props> {
+  
+  state = {
+  	authCode: ''
+  }
+
+  onChangeText(authCode) {
+  	this.setState( { authCode })
+  	//this.setState({ // another way of doing the same thing!
+  	//	authCode: authCode
+  	//})
+  }
+
+  signUp() {
+  	Auth.signUp( {
+  		userName: 'User name',
+  		password: 'password',
+  		attributes: {
+  			phoneNumber: '+12345678912'
+  			email: 'email@gmail.com'
+  		}
+  	})
+  	.then(res => {
+  		console.log('Successful signup: ', res)
+  	})
+  	.catch(err => {
+  		console.log('error signing up: ', err)
+  	})
+  }
+
+  confirmUser() {
+  	const { authCode } = this.state
+  	Auth.confirmSignUp('User name', authCode)
+  		.then(res => {
+  			console.log('successful confirmation: ', res)
+  		})
+  		.catch(err => {
+  			console.log('error confirming user: ', err)
+  		})
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <Button title='Sign Up' onPress={this.signUp.bind(this)} />
+        <TextInput
+        placeholder='Input Code'
+        onChangeText={value => this.onChangeText(value)}
+        style={styles.input} />
+        <Button
+        title='Confirm User'
+        onPress={this.confirmUser.bind(this)} />
       </View>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create({
+  input: {
+    height: 50,
+    backgroundColor: '#ededed',
+    marginVertical: 10
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+    backgroundColor: '#F5FCFF'
+  }
+})
